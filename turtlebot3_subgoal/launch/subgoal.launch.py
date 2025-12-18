11b09976945ba30c73479ae7f8c1d5ce582520e5
Mateus Pincho <mateus.oliveira@ee.ufcg.edu.br>
2025-12-18 12:04:06 -0300

    add: define the purpose of the subgoal.launch.py file

---
+++ turtlebot3_subgoal/launch/subgoal.launch.py
@@ -0,0 +1,8 @@
+'''
+Este launch deve: 
+- Iniciar o SLAM -> usar o launch do slam de turtlebot3_cartographer
+- Iniciar o no de distancia do Lidar
+- Iniciar o fuzzy planner para gerar subgoal
+- Iniciar o go to goal para publicar o cmd vel
+'''
+
